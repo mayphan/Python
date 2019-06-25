1c07a6ad7fded14ddfc6f0b3584a83d4ae1bed06
mayphan <47878485+mayphan@users.noreply.github.com>
2019-06-25 00:12:06 -0700

    Delete 3464_log.py

--- 3464_log.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import os
-
-time_stmp=datetime.datetime.now().strftime('%m-%d-%Y-%H-%M-%S')
-f_nam = datetime.datetime.now().strftime('%m-%d-%Y')
-print((time_stmp))
-os.system("touch 3464_log"+f_nam)
-
